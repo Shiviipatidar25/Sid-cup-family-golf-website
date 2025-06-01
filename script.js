fec24695e7507ca0e4850b4ea8441001341b00a0
shivani <patidarshivani27@gmail.com>
2025-06-01 15:36:29 +0530

    added footer

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ document.addEventListener("mousemove",function(dets){
 
 })
 
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        cur.style.scale = 3
+        cur.style.border = "0.25px solid #fff"
+        cur.style.backgroundColor = "tranparent"
+    })
+
+     elem.addEventListener("mouseleave",function(){
+        cur.style.scale = 1
+        cur.style.border = "0px solid #95c11e"
+        cur.style.backgroundColor = "tranparent"
+    })
+
+})
+
 
 
 
@@ -35,9 +51,77 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
+        //markers:true,
         start:"top -25%",
         end:"top -70%",
         scrub:2
+    }
+})
+
+gsap.from("#about-us img, #about-us-in",{
+   y:90,
+   opacity:0,
+   duration:1,
+   scrollTrigger:{
+    trigger:"#about-us",
+    scroller:"body",
+   // markers:true,
+    start:"top 70%",
+    end:"top 65%",
+    scrub:3,
+   }
+
+});
+
+gsap.from(".card",{
+   scale:0.8,
+   opacity:0,
+   duration:1,
+   scrollTrigger:{
+    trigger:".card",
+    scroller:"body",
+   // markers:true,
+    start:"top 70%",
+    end:"top 65%",
+    scrub:2
+   }
+
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+      //  markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+       // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        start:"top 75%",
+        end:"top 75%",
+        scrub:3,
+
     }
 })
